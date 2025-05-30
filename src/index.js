743029b6050d43f2f6e2f4273943aa16d6c79e35
yijenislearningtocode <yijenhs@gmail.com>
2025-05-30 21:26:56 +0200

    incorporated API to get weather

---
+++ src/index.js
@@ -0,0 +1,27 @@
+function searchCity(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#cityInput");
+  let city = cityInputElement.value;
+
+  let apiKey = "08dc473fat5683eb45d51fcd6703o645";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemp);
+}
+
+function showTemp(response) {
+  let tempElement = document.querySelector("#nowTempValue");
+  let cityElement = document.querySelector("#cityNow");
+  let humidityElement = document.querySelector("#humidityNow");
+  let windElement = document.querySelector("#windNow");
+  let iconElement = document.querySelector("#iconNow");
+
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(response.data.temperature.current);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"/>`;
+}
+
+let searchWeatherForm = document.querySelector("#weatherSearch");
+searchWeatherForm.addEventListener("submit", searchCity);
