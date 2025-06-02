ac004d29bde1f0e7a2b5385e1a2211e7aaab3429
yijenislearningtocode <yijenhs@gmail.com>
2025-06-02 19:33:54 +0200

    Added description from API

--- src/index.js
+++ src/index.js
@@ -15,12 +15,14 @@ function showTemp(response) {
   let humidityElement = document.querySelector("#humidityNow");
   let windElement = document.querySelector("#windNow");
   let iconElement = document.querySelector("#iconNow");
+  let descriptionElement = document.querySelector("#descriptionNow");
 
   cityElement.innerHTML = response.data.city;
   tempElement.innerHTML = Math.round(response.data.temperature.current);
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"/>`;
+  descriptionElement.innerHTML = response.data.condition.description;
 }
 
 let searchWeatherForm = document.querySelector("#weatherSearch");
