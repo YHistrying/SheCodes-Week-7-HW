47d23d9c3c195d8489edb0900a48ef1a2dd873e4
yijenislearningtocode <yijenhs@gmail.com>
2025-06-02 20:15:21 +0200

    Made time functional on the page

--- src/index.js
+++ src/index.js
@@ -16,14 +16,39 @@ function showTemp(response) {
   let windElement = document.querySelector("#windNow");
   let iconElement = document.querySelector("#iconNow");
   let descriptionElement = document.querySelector("#descriptionNow");
+  let dateElement = document.querySelector("#dateNow");
+  let dateTime = new Date(response.data.time * 1000);
+  console.log(dateTime);
 
   cityElement.innerHTML = response.data.city;
   tempElement.innerHTML = Math.round(response.data.temperature.current);
-  humidityElement.innerHTML = `${response.data.temperature.humidity} %`;
-  windElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"/>`;
   descriptionElement.innerHTML = response.data.condition.description;
+  dateElement.innerHTML = getDateTime(dateTime);
 }
 
+function getDateTime(dateTime) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dateTime.getDay()];
+  let minutes = dateTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let hours = dateTime.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
 let searchWeatherForm = document.querySelector("#weatherSearch");
 searchWeatherForm.addEventListener("submit", searchCity);
